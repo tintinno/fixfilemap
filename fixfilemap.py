6f6710f700493b61bf3dd29d36f08b2b5e649f64
tintinno <noemail@example.com>
2016-09-29 11:56:35 -0400

    added support for glossaries

--- fixfilemap.py
+++ fixfilemap.py
@@ -1,6 +1,10 @@
 #!/bin/python
 
 from bs4 import BeautifulSoup
+<<<<<<< HEAD
+from lxml import etree
+=======
+>>>>>>> 56b9d9cdd233a7d0f1547e599431de1256286c8d
 from os import path, chdir
 from sys import version_info
 
@@ -8,53 +12,91 @@ homedir = path.expanduser('~')
 importdir = path.join(homedir, 'Desktop/import')
 
 if version_info.major > 2:
+<<<<<<< HEAD
+    raise SystemExit('Use Python 2.')
+
+# tests
+try:
+    chdir(importdir)
+=======
 	raise SystemExit('Use Python 2.')
 
 # tests
 try:
 	chdir(importdir)
+>>>>>>> 56b9d9cdd233a7d0f1547e599431de1256286c8d
 except:
-	raise SystemExit('Create /Desktop/import')
+    raise SystemExit('Create /Desktop/import')
 
 try:
-	with open('filemap.xml','r') as f:
+    with open('filemap.xml','r') as f:
 		filemap = f.read()
 except:
-	raise SystemExit('Cannot open filemap.xml')
+    raise SystemExit('Cannot open filemap.xml')
 
 # functions to create/add ishfield
 def ishmaker(name,level,string):
-	ishfield = soup.new_tag('ishfield')
-	ishfield['name'] = name
-	ishfield['level'] = level
-	ishfield.string = string
-	return ishfield
+    ishfield = soup.new_tag('ishfield')
+    ishfield['name'] = name
+    ishfield['level'] = level
+    ishfield['xml:space'] = 'preserve' # new to Trisoft 11
+    ishfield.string = string
+    return ishfield
 
 def addishfield(soup,ish,ishfield):
-	last = ish('ishfield')[2]
-	last.insert_after('\n')
-	last.next_sibling.insert_after(ishfield)
-	return soup
+    last = ish('ishfield')[2]
+    last.insert_after('\n')
+    last.next_sibling.insert_after(ishfield)
+    return soup
 
-def gettype(soup):
-	if soup.title.parent.name == 'concept':
-		string = 'Concept'
-	elif soup.title.parent.name == 'task':
-		string = 'Task'
-	elif soup.title.parent.name == 'reference':
-		string = 'Reference'
-	else:
-		raise SystemExit('Topic type error: %s' % str(soup.title.string))
-	return string
-	
+def getrootelement(filepath):
+    with open(filepath,'r') as f:
+        doc = f.read()
+    root = etree.fromstring(doc).tag
+    if root[:5] == 'gloss':
+        root = root[:5] + ' ' + root[5:]
+    return root.title()
+
 # main
 soup = BeautifulSoup(filemap,'html.parser')
 ishes = soup('ishfields')
 
 for ish in ishes:
-	# if we have less than 4 ishfield elements, the SDL conversion failed
-	if len(ish('ishfield')) < 4:
+    
+    #ish.parent.parent['folderhasmixedcontent'] = 'False'
+
+    # if we have less than 4 ishfield elements, the SDL conversion failed
+    if len(ish('ishfield')) < 4:
 	
+<<<<<<< HEAD
+        # if parent element's ishtype value is 'ISHIllustration' then we
+        # need to add the ishfield for images, using Default as the string
+        if ish.parent['ishtype'] == 'ISHIllustration':
+            ish.parent.parent['targetfolder'] = "Import\\images"
+            ishfield = ishmaker('FRESOLUTION','lng','Default')
+            soup = addishfield(soup,ish,ishfield)
+
+        # if parent element's ishtype value is 'ISHMasterDoc' then we have
+        # a map, so use Map as the string
+        elif ish.parent['ishtype'] == 'ISHMasterDoc':
+            ish.parent.parent['targetfolder'] = "Import\\maps"
+            root = getrootelement(ish.parent.parent['filepath'])
+            ishfield = ishmaker('FMASTERTYPE','logical',root)
+            soup = addishfield(soup,ish,ishfield)
+		
+        # if parent element's ishtype value is 'ISHModule' then we have a
+        # normal topic. Open the file referenced via 'filepath' attribute
+        # and determine it's topic type
+        elif ish.parent['ishtype'] == 'ISHModule':
+            ish.parent.parent['targetfolder'] = "Import\\topics"
+            root = getrootelement(ish.parent.parent['filepath'])
+            ishfield = ishmaker('FMODULETYPE','logical',root)
+            soup = addishfield(soup,ish,ishfield)
+		
+# Use Default as FRESOLUTION value
+for ish in soup('ishfield',{'name':'FRESOLUTION'}):
+    ish.string = "Default"
+=======
 		# if parent element's ishtype value is 'ISHIllustration' then we
 		# need to add the ishfield for images, using Default as the string
 		if ish.parent['ishtype'] == 'ISHIllustration':
@@ -89,6 +131,7 @@ for ish in ishes:
 			ttype = gettype(docsoup)
 			ishfield = ishmaker('FMODULETYPE','logical',ttype)
 			soup = addishfield(soup,ish,ishfield)
+>>>>>>> 56b9d9cdd233a7d0f1547e599431de1256286c8d
 
 # Use Default as FRESOLUTION value
 for ish in soup('ishfield',{'name':'FRESOLUTION'}):
@@ -97,4 +140,4 @@ for ish in soup('ishfield',{'name':'FRESOLUTION'}):
 # write changes to file
 content = str(soup)
 with open('filemap.xml','w') as out:
-	out.write(content)
+    out.write(content)
